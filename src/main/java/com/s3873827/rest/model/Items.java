4f60d79251d8f8c8488235c493f317fb28af636b
ashmit-sachan <ashmit666@gmail.com>
2022-09-09 13:48:15 +1000

    Items Model Created

--- src/main/java/com/s3873827/rest/model/Items.java
+++ src/main/java/com/s3873827/rest/model/Items.java
@@ -1,4 +1,22 @@
 package com.s3873827.rest.model;
 
-public class Items {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Items
+{
+    private List<Item> itemList;
+
+    public List<Item> getItemList()
+    {
+        if (itemList == null)
+            itemList = new ArrayList<>();
+
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
 }
